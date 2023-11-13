013413f051b73e233d548f36762e5b0b1f4b8490
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-13 14:53:07 +0900

    feat: 할인 전 총주문 금액 계산 추가

--- src/controller/OrderController.js
+++ src/controller/OrderController.js
@@ -1,17 +1,36 @@
 import InputView from "../InputView.js";
 import Calculator from "../utils/Calculator.js";
+import { Appetizer, MainDish, Drink, Dessert } from "../Model/Menu.js";
+import { MENU_INFO } from "../constant/Constant.js";
 
 class OrderController {
   #visitDate;
   #isWeekend;
+  #servedMenus;
   #orderInfo;
+  #totalSum;
 
-  constructor() {}
+  constructor() {
+    const appetizers = MENU_INFO.filter(
+      (menu) => menu.category === "애피타이저"
+    ).map((menu) => new Appetizer(menu.name, menu.price));
+    const mainDishes = MENU_INFO.filter((menu) => menu.category === "메인").map(
+      (menu) => new MainDish(menu.name, menu.price)
+    );
+    const desserts = MENU_INFO.filter((menu) => menu.category === "디저트").map(
+      (menu) => new Dessert(menu.name, menu.price)
+    );
+    const drinks = MENU_INFO.filter((menu) => menu.category === "음료").map(
+      (menu) => new Drink(menu.name, menu.price)
+    );
+    this.#servedMenus = [...appetizers, ...mainDishes, ...desserts, ...drinks];
+  }
 
   async placeOrder() {
     await this.setVisitDate();
     this.setIsWeekend(this.#visitDate);
-    this.setOrderInfo();
+    await this.setOrderInfo();
+    this.calculateTotalSum();
   }
 
   async setVisitDate() {
@@ -24,6 +43,16 @@ class OrderController {
 
   async setOrderInfo() {
     this.#orderInfo = await InputView.readMenu();
+  }
+
+  calculateTotalSum() {
+    this.#orderInfo.forEach((qty, name) => {
+      this.#totalSum += this.getMenuInfo(name).price * qty;
+    });
+  }
+
+  getMenuInfo(name) {
+    return this.#servedMenus.find((menu) => menu.name === name);
   }
 }
 
