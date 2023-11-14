47309b5c88493f06e2cf3b682392ca35b0695c04
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-14 11:46:59 +0900

    feat: 결과 출력 관련 상수 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,12 +1,31 @@
 import { Console } from "@woowacourse/mission-utils";
-import { ERROR_MESSAGE, RESULT_HEADER } from "./constant/Constant.js";
+import {
+  ERROR_MESSAGE,
+  RESULT_HEADER,
+  ALERT_MESSAGE,
+  UNIT,
+  NOTHING,
+} from "./constant/Constant.js";
+import Converter from "./utils/Converter.js";
 
 const OutputView = {
-  printMenu() {
-    Console.print(RESULT_HEADER.menu);
-  },
   printErrorMessage(error) {
     Console.print(error.message);
+  },
+
+  printGreetings() {
+    Console.print(ALERT_MESSAGE.greetings);
+  },
+
+  printResultHeader(date) {
+    Console.print(ALERT_MESSAGE.resultHeader(date));
+  },
+
+  printMenu(menuInfo) {
+    Console.print(RESULT_HEADER.menu);
+    menuInfo.forEach((qty, name) => {
+      Console.print(name + " " + UNIT.quantity(qty));
+    });
   },
 };
 
