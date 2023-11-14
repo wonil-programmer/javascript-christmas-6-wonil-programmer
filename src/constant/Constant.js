891a3c158bdac1daaed8b98c78ddf3d0f28d7767
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-14 11:52:18 +0900

    feat: 출력 관련 상수 추가

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -28,34 +28,42 @@ export const BADGE = Object.freeze({
 });
 
 export const EVENT_LIST = Object.freeze({
-  xMasDDay: "크리스마스 디데이 할인",
-  weekday: "평일 할인",
-  weekend: "주말 할인",
-  special: "특별 할인",
-  gift: "증정 이벤트",
+  xMasDDay: "크리스마스 디데이 할인: ",
+  weekday: "평일 할인: ",
+  weekend: "주말 할인: ",
+  special: "특별 할인: ",
+  gift: "증정 이벤트: ",
 });
 
 export const ALERT_MESSAGE = Object.freeze({
-  greetings: "안녕하세요! 우테코 식당 {12}월 이벤트 플래너입니다.\n",
-  result: "{12}월 {3}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n",
+  greetings: "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.",
+  resultHeader: (date) =>
+    `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
 });
 
 export const ASK_MESSAGE = Object.freeze({
-  greetings: "안녕하세요! 우테코 식당 {12}월 이벤트 플래너입니다.\n",
   visitDate:
-    "{12}월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n",
+    "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n",
   menuInfo:
     "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n",
 });
 
 export const RESULT_HEADER = Object.freeze({
   menu: "<주문 메뉴>",
-  totalPriceBeforeDiscount: "<할인 전 총주문 금액>",
-  benefitHistory: "<혜택 내역>",
-  totalBenefitAmount: "<총혜택 금액>",
-  finalPrice: "<할인 후 예상 결제 금액>",
-  badge: "<{12}월 이벤트 배지>",
+  totalSum: "\n<할인 전 총주문 금액>",
+  gift: "\n<증정 메뉴>",
+  benefitHistory: "\n<혜택 내역>",
+  totalBenefit: "\n<총혜택 금액>",
+  payment: "\n<할인 후 예상 결제 금액>",
+  badge: "\n<12월 이벤트 배지>",
 });
+
+export const UNIT = Object.freeze({
+  price: (price) => `${price}원`,
+  quantity: (qty) => `${qty}개`,
+});
+
+export const NOTHING = "없음";
 
 const PREFIX_ERROR = "[ERROR]";
 
