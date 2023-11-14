f45791f88ec84a7ea7916327582be66fab20ad51
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-14 11:51:08 +0900

    Revert "feat: 결과 출력 관련 상수 추가"
    This reverts commit 47309b5c88493f06e2cf3b682392ca35b0695c04.

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,31 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
-import {
-  ERROR_MESSAGE,
-  RESULT_HEADER,
-  ALERT_MESSAGE,
-  UNIT,
-  NOTHING,
-} from "./constant/Constant.js";
-import Converter from "./utils/Converter.js";
+import { ERROR_MESSAGE, RESULT_HEADER } from "./constant/Constant.js";
 
 const OutputView = {
+  printMenu() {
+    Console.print(RESULT_HEADER.menu);
+  },
   printErrorMessage(error) {
     Console.print(error.message);
-  },
-
-  printGreetings() {
-    Console.print(ALERT_MESSAGE.greetings);
-  },
-
-  printResultHeader(date) {
-    Console.print(ALERT_MESSAGE.resultHeader(date));
-  },
-
-  printMenu(menuInfo) {
-    Console.print(RESULT_HEADER.menu);
-    menuInfo.forEach((qty, name) => {
-      Console.print(name + " " + UNIT.quantity(qty));
-    });
   },
 };
 
