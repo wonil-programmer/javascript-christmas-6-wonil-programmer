b3be7334d1361bf205fde0b61d1923b751d6966b
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-14 21:34:21 +0900

    refactor: 제공 메뉴 상수 가격 포맷 변경

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -4,18 +4,18 @@ export const MONTH_START_DATE = 1;
 export const MONTH_END_DATE = 31;
 
 export const MENU_INFO = [
-  { name: "양송이수프", price: 6000, category: "애피타이저" },
-  { name: "타파스", price: 5500, category: "애피타이저" },
-  { name: "시저샐러드", price: 8000, category: "애피타이저" },
-  { name: "티본스테이크", price: 55000, category: "메인" },
-  { name: "바비큐립", price: 54000, category: "메인" },
-  { name: "해산물파스타", price: 35000, category: "메인" },
-  { name: "크리스마스파스타", price: 25000, category: "메인" },
-  { name: "초코케이크", price: 15000, category: "디저트" },
-  { name: "아이스크림", price: 5000, category: "디저트" },
-  { name: "제로콜라", price: 3000, category: "음료" },
-  { name: "레드와인", price: 60000, category: "음료" },
-  { name: "샴페인", price: 25000, category: "음료" },
+  { name: "양송이수프", price: 6_000, category: "애피타이저" },
+  { name: "타파스", price: 5_500, category: "애피타이저" },
+  { name: "시저샐러드", price: 8_000, category: "애피타이저" },
+  { name: "티본스테이크", price: 55_000, category: "메인" },
+  { name: "바비큐립", price: 54_000, category: "메인" },
+  { name: "해산물파스타", price: 35_000, category: "메인" },
+  { name: "크리스마스파스타", price: 25_000, category: "메인" },
+  { name: "초코케이크", price: 15_000, category: "디저트" },
+  { name: "아이스크림", price: 5_000, category: "디저트" },
+  { name: "제로콜라", price: 3_000, category: "음료" },
+  { name: "레드와인", price: 60_000, category: "음료" },
+  { name: "샴페인", price: 25_000, category: "음료" },
 ];
 
 export const MENU_CATEGORY = Object.freeze({
@@ -82,6 +82,7 @@ export const NOTHING = "없음";
 const PREFIX_ERROR = "[ERROR]";
 
 export const ERROR_MESSAGE = Object.freeze({
+  invalidCategory: `${PREFIX_ERROR} 유효하지 않은 종류입니다.`,
   invalidDate: `${PREFIX_ERROR} 유효하지 않은 날짜입니다. 다시 입력해 주세요.`,
   invalidOrder: `${PREFIX_ERROR} 유효하지 않은 주문입니다. 다시 입력해 주세요.`,
 });
