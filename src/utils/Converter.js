30a27748c2504b2a0896597f320f21387dd0ce11
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-14 12:05:58 +0900

    feat: 음수변환 함수 추가

--- src/utils/Converter.js
+++ src/utils/Converter.js
@@ -2,6 +2,10 @@ const Converter = {
   applyNumberFormat(number) {
     return new Intl.NumberFormat("ko-KR").format(number);
   },
+
+  applyNegative(number) {
+    return "-" + number;
+  },
 };
 
 export default Converter;
