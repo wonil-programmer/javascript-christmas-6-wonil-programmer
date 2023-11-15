542754a4e693a7d1c9d2f7c55d5696ea49f2bbfc
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-15 20:22:27 +0900

    fix: 테스트 파일 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,11 +1,33 @@
 import App from "../src/App.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
 import { EOL as LINE_SEPARATOR } from "os";
-import {
-  mockQuestions,
-  getLogSpy,
-  getOutput,
-  expectLogContains,
-} from "../__testUtils__/testUtils.js";
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+
+    return Promise.resolve(input);
+  });
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+
+  return logSpy;
+};
+
+const getOutput = (logSpy) => {
+  return [...logSpy.mock.calls].join(LINE_SEPARATOR);
+};
+
+const expectLogContains = (received, expectedLogs) => {
+  expectedLogs.forEach((log) => {
+    expect(received).toContain(log);
+  });
+};
 
 describe("기능 테스트", () => {
   test("모든 타이틀 출력", async () => {
