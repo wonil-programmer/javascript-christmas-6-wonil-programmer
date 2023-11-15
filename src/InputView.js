17ac5e310f0ddf69857762539588363c6065608d
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-15 22:14:21 +0900

    refactor: 입력 함수 공통 로직 분리

--- src/InputView.js
+++ src/InputView.js
@@ -5,32 +5,35 @@ import OutputView from "./OutputView.js";
 import Order from "./model/Order.js";
 
 const InputView = {
-  async readDate() {
+  async readInput(promptMessage, validationFunction, processFunction) {
     while (true) {
       try {
-        const date = await Console.readLineAsync(ASK_MESSAGE.visitDate);
-        Validator.validateDate(date);
-
-        return Number(date);
+        const userInput = await Console.readLineAsync(promptMessage);
+        validationFunction(userInput);
+        return processFunction(userInput);
       } catch (error) {
         OutputView.printErrorMessage(error);
       }
     }
   },
 
-  async readOrder(menuInfo) {
-    while (true) {
-      try {
-        const orderInput = await Console.readLineAsync(ASK_MESSAGE.menuInfo);
-        Validator.validateOrderFormat(orderInput);
-        const orderedMenus = orderInput.split(SEPARATOR.comma);
-        const order = new Order(orderedMenus, menuInfo);
+  async readDate() {
+    const promptMessage = ASK_MESSAGE.visitDate;
+    const validationFunction = Validator.validateDate;
+    const processFunction = (date) => Number(date);
 
-        return order;
-      } catch (error) {
-        OutputView.printErrorMessage(error);
-      }
-    }
+    return this.readInput(promptMessage, validationFunction, processFunction);
+  },
+
+  async readOrder(menuInfo) {
+    const promptMessage = ASK_MESSAGE.menuInfo;
+    const validationFunction = Validator.validateOrderFormat;
+    const processFunction = (orderInput) => {
+      const orderedMenus = orderInput.split(SEPARATOR.comma);
+      return new Order(orderedMenus, menuInfo);
+    };
+
+    return this.readInput(promptMessage, validationFunction, processFunction);
   },
 };
 
