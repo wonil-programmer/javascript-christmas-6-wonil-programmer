d5f69da2c2fe73c9eca16f241086c0c138ff2c2d
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-15 18:55:50 +0900

    refactor: 주문 컨트롤러 로직 변경
    주문 클래스 추가에 따른 실행로직 변경

--- src/controller/OrderController.js
+++ src/controller/OrderController.js
@@ -1,109 +1,59 @@
 import InputView from "../InputView.js";
-import Calculator from "../utils/Calculator.js";
+import OutputView from "../OutputView.js";
 import {
-  SPECIAL_DATE,
   EVENT,
   GIFT_LIST,
   BADGE,
   BADGE_CUT_OFF,
-  DATE,
-  MENU_CATEGORY,
 } from "../constant/Constant.js";
 import Event from "../Event.js";
-import OutputView from "../OutputView.js";
+import Calculator from "../utils/Calculator.js";
 
 class OrderController {
   #menuInfo;
-  #orderInfo = new Map();
-  #totalSum;
-  #totalDiscount;
   #benefitHistory = new Map();
-  #totalBenefit;
+  #totalDiscount = 0;
   #gift = new Map();
   #badge;
 
   constructor(preparedMenus) {
     this.#menuInfo = [...preparedMenus];
-    this.#totalSum = 0;
-    this.#totalBenefit = 0;
   }
 
   async placeOrder(visitDate) {
-    const isWeekend = Calculator.calculateIsWeekend(visitDate);
-    await this.setOrderInfo();
-    this.calculateTotalSum();
-    this.#totalSum >= 10_000 && this.calculateDiscount(visitDate, isWeekend);
-    this.chooseGift();
-    this.calculateTotalBenefit();
-    this.rewardBadge(this.#totalBenefit);
+    const order = await InputView.readOrder(this.#menuInfo);
+    const totalPrice = order.calculateTotalPrice();
+    if (totalPrice > 10_000) {
+      this.calculateDiscount(visitDate, order);
+      this.chooseGift(totalPrice);
+    }
+    const totalBenefit = this.calculateTotalBenefit();
+    this.rewardBadge(totalBenefit);
+    OutputView.printResultHeader(visitDate);
+    this.printOrderInfo(order, totalPrice, totalBenefit);
   }
 
-  async setOrderInfo() {
-    this.#orderInfo = await InputView.readMenu();
-  }
-
-  calculateTotalSum() {
-    this.#orderInfo.forEach((qty, name) => {
-      this.#totalSum += this.getMenuInfo(name).price * qty;
-    });
-  }
-
-  countMainDish() {
-    let mainDishCount = 0;
-    this.#orderInfo.forEach((qty, name) => {
-      if (this.getMenuInfo(name).category === MENU_CATEGORY.main) {
-        mainDishCount += qty;
-      }
-    });
-
-    return mainDishCount;
-  }
-
-  countDessert() {
-    let dessertCount = 0;
-    this.#orderInfo.forEach((qty, name) => {
-      if (this.getMenuInfo(name).category === MENU_CATEGORY.dessert) {
-        dessertCount += qty;
-      }
-    });
-
-    return dessertCount;
-  }
-
-  getMenuInfo(name) {
-    return this.#menuInfo.find((menu) => menu.name === name);
-  }
-
-  setBenefitHistory = (eventName, benefit) => {
+  setBenefitHistory(eventName, benefit) {
     if (benefit > 0) {
       this.#benefitHistory.set(eventName, benefit);
     }
-  };
-
-  applyDiscount = (eventName, discountFunction, ...args) => {
-    const discount = discountFunction(...args);
-    this.setBenefitHistory(eventName, discount);
-    this.#totalBenefit += discount;
-  };
-
-  calculateDiscount(visitDate, isWeekend) {
-    if (visitDate <= DATE.xMasDate)
-      this.applyDiscount(EVENT.xMasDDay, Event.applyXMasDDay, visitDate);
-    if (SPECIAL_DATE.includes(visitDate))
-      this.applyDiscount(EVENT.special, Event.applySpecial);
-    if (isWeekend) {
-      const mainDishQty = this.countMainDish();
-      this.applyDiscount(EVENT.weekend, Event.applyWeekend, mainDishQty);
-    } else {
-      const dessertQty = this.countDessert();
-      this.applyDiscount(EVENT.weekday, Event.applyWeekday, dessertQty);
-    }
   }
 
-  chooseGift() {
+  calculateDiscount(visitDate, order) {
+    const applyAndRecordDiscount = (event, discount) => {
+      this.#totalDiscount += discount;
+      this.setBenefitHistory(event, discount);
+    };
+    applyAndRecordDiscount(EVENT.xMasDDay, Event.applyXMasDDay(visitDate));
+    applyAndRecordDiscount(EVENT.weekday, Event.applyWeekday(visitDate, order));
+    applyAndRecordDiscount(EVENT.weekend, Event.applyWeekend(visitDate, order));
+    applyAndRecordDiscount(EVENT.special, Event.applySpecial(visitDate));
+  }
+
+  chooseGift(totalPrice) {
     let giftPrice = 0;
     GIFT_LIST.forEach((gift) => {
-      if (gift.conditionPrice <= this.#totalSum) {
+      if (gift.conditionPrice <= totalPrice) {
         this.#gift.set(gift.name, gift.price);
         giftPrice += gift.price;
       }
@@ -112,33 +62,36 @@ class OrderController {
   }
 
   calculateTotalBenefit() {
+    let totalBenefit = 0;
     for (let benefit of this.#benefitHistory.values()) {
-      this.#totalBenefit += benefit;
+      totalBenefit += benefit;
     }
+    return totalBenefit;
   }
 
-  rewardBadge(benefit) {
-    if (benefit >= BADGE_CUT_OFF.santa) {
+  rewardBadge(totalBenefit) {
+    if (totalBenefit >= BADGE_CUT_OFF.santa) {
       this.#badge = BADGE.santa;
       return;
     }
-    if (benefit >= BADGE_CUT_OFF.tree) {
+    if (totalBenefit >= BADGE_CUT_OFF.tree) {
       this.#badge = BADGE.tree;
       return;
     }
-    if (benefit >= BADGE_CUT_OFF.star) {
+    if (totalBenefit >= BADGE_CUT_OFF.star) {
       this.#badge = BADGE.star;
       return;
     }
   }
 
-  printOrderInfo() {
-    OutputView.printMenu(this.#orderInfo);
-    OutputView.printTotalSum(this.#totalSum);
+  printOrderInfo(order, totalPrice, totalBenefit) {
+    OutputView.printMenu(order);
+    OutputView.printTotalPrice(totalPrice);
     OutputView.printGift(this.#gift);
     OutputView.printBenefitHistory(this.#benefitHistory);
-    OutputView.printTotalBenefit(this.#totalBenefit);
-    OutputView.printPayment(this.#totalSum, this.#totalDiscount);
+    OutputView.printTotalBenefit(totalBenefit);
+    const payment = Calculator.substractTwoNum(totalPrice, this.#totalDiscount);
+    OutputView.printPayment(payment);
     OutputView.printBadge(this.#badge);
   }
 }
